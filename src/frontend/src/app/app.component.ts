8e847a02b7d74d2904c8a33b96382d5c95604898
nitinlal <nklal4@gmail.com>
2020-05-22 16:54:13 -0700

    feat: add chart

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,10 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private http: HttpClient) {}
+  // stats
+  stats = this.http.get(``);
+
+  public pieChartLabels: string[] = [
+    'Chrome',
+    'Safari',
+    'Firefox',
+    'Internet Explorer',
+    'Other',
+  ];
+  public pieChartData: number[] = [40, 20, 20, 10, 10];
+  public pieChartType: string = 'pie';
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
 }
--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -4,10 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { ChartsModule } from 'ng2-charts';
+import 'hammerjs';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    ChartsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
