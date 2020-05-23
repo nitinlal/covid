c5288d3aa613c3747da15eeaff5f5dd8b61108fa
nitinlal <nklal4@gmail.com>
2020-05-23 06:11:45 -0700

    feat: set up basic form

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,51 +1,88 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import gql from 'graphql-tag';
+import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      userstat: this.fb.array([]),
+    });
+  }
+
   title = 'frontend';
+  stats = [
+    { stat: 'total_cases' },
+    { stat: 'recovery_cases' },
+    { stat: 'death_cases' },
+    { stat: 'currently_infected' },
+  ];
+  myForm: FormGroup;
 
   constructor(
     private http: HttpClient,
     private apollo: Apollo,
     httpLink: HttpLink,
+    private fb: FormBuilder,
   ) {
     apollo.create({
       link: httpLink.create({ uri: 'http://localhost:3000/graphql' }),
-      cache: new InMemoryCache(),
+      cache: new InMemoryCache({
+        resultCaching: false,
+        freezeResults: false,
+      }),
     });
 
     this.getStats();
   }
   // stats
-  stats;
+  statsRes;
 
-  public getStats = () => {
+  public getStats = (val = 'total_cases') => {
+    console.log('stats called', val);
     this.apollo
       .query({
-        query: gql`
-          {
-            stats {
-              data {
-                total_cases
-              }
+        query: gql`              {
+          stats {
+            data {
+              ${val}
             }
           }
-        `,
+        }`,
       })
       .subscribe(result => {
-        this.stats = result.data;
-        console.log(this.stats);
+        this.statsRes = result.data;
+        console.log(this.statsRes.stats);
       });
   };
+
+  onChange(stat: string, isChecked: boolean) {
+    console.log('onchange called');
+    const statFormArray = <FormArray>this.myForm.controls.userstat;
+
+    if (isChecked) {
+      statFormArray.push(new FormControl(stat));
+    } else {
+      let index = statFormArray.controls.findIndex(x => x.value == stat);
+      statFormArray.removeAt(index);
+    }
+
+    const cases = statFormArray.getRawValue().reduce((acc, curr) => {
+      acc = acc + ' ' + curr;
+      return acc;
+    }, '');
+
+    console.log({ cases });
+    this.getStats(cases);
+  }
 
   public pieChartLabels: string[] = [
     'Chrome',
--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import { ChartsModule } from 'ng2-charts';
-import 'hammerjs';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ApolloModule } from 'apollo-angular';
 import { HttpLinkModule } from 'apollo-angular-link-http';
+import 'hammerjs';
+import { ChartsModule } from 'ng2-charts';
+import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,6 +21,8 @@ import { HttpLinkModule } from 'apollo-angular-link-http';
     HttpClientModule,
     ApolloModule,
     HttpLinkModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
